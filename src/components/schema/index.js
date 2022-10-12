f1eea2388a1215a1b7fdfcc476eb9cdf3008cbec
Avi <avip3513@gmail.com>
2022-10-12 21:18:41 +0530

    form validation

---
+++ src/components/schema/index.js
@@ -0,0 +1,12 @@
+import * as Yup from "yup";
+
+export const signupSchema = Yup.object({
+  name: Yup.string().min(2).max(10).required("please enter your name"),
+
+  email: Yup.string().email().required("Please enter email"),
+
+  password: Yup.string()
+    .min(6, "Password must be 6 characters")
+    .required("Please Enter Password"),
+
+});
